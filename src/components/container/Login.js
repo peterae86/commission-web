880a4176f88f6db488d5e0406ffd1640d2b24cd4
lingjuanguan <lingjuanguan@gmail.com>
2017-08-24 23:32:15 +0800

    提交代码

--- src/components/container/Login.js
+++ src/components/container/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '../component/Button/Button';
+const logo = require("../../images/logo.png");
 class Login extends React.Component {
     constructor(props) {
         super(props);
@@ -8,7 +9,18 @@ class Login extends React.Component {
     render() {
         return (
             <div className="login-container">
-                <Button value="测试"/>
+                <div className="login-title">
+                    <img src={logo}/>
+                </div>
+                <div className="login-body">
+                    <div className="login-panel-container">
+                        <div className="login-panel">
+                            <div className="panel-title">密码登录</div>
+                            <Button value="登录" styleName="btn-middle"/>
+                        </div>
+                    </div>
+                </div>
+               
             </div>
         );
     }
