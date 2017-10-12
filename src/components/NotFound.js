5cc44d9606adf2fdc2ccc70d408a6fa3f0d6f54a
xiaorui.guo <775870132@qq.com>
2017-10-12 23:36:03 +0800

    404

---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import background from '../images/404.jpg';
+import '../styles/notFound.scss'
+class NotFound extends React.Component{
+    render(){
+        return <div className="container404">
+            <div className="container404-body">
+            </div>
+        </div>
+    }
+}
+export default NotFound
