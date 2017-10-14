9ff6fb95463f6f68b15094e08ecc0a1edabc29de
xiaorui.guo <775870132@qq.com>
2017-10-14 15:08:04 +0800

    fix

--- src/components/container/import/UserRelation.js
+++ src/components/container/import/UserRelation.js
@@ -5,13 +5,13 @@ class UserRelation extends ImportListPage{
         this.state.table.config={
             column: [
                 {name: "公司编码", key: "corpCode", textAlign: "center", width: "10%"},
-                {name: "经纪人姓名", key: "corpCode", textAlign: "center", width: "13%"},
-                {name: "经纪人系统号", key: "corpName", textAlign: "center", width: "13%"},
-                {name: "师傅姓名", key: "regionCode", textAlign: "center", width: "10%"},
-                {name: "师傅系统号", key: "regionName", textAlign: "center", width: "10%"},
-                {name: "作战组长姓名", key: "storeCode", textAlign: "center", width: "11%"},
-                {name: "组长系统号", key: "storeName", textAlign: "center", width: "11%"},
-                {name: "担保方式", key: "userCode", textAlign: "center", width: "10%"},
+                {name: "经纪人姓名", key: "userCode", textAlign: "center", width: "13%"},
+                {name: "经纪人系统号", key: "userName", textAlign: "center", width: "13%"},
+                {name: "师傅姓名", key: "masterUserName", textAlign: "center", width: "10%"},
+                {name: "师傅系统号", key: "masterUserCode", textAlign: "center", width: "10%"},
+                {name: "作战组长姓名", key: "teamLeaderUserName", textAlign: "center", width: "11%"},
+                {name: "组长系统号", key: "teamLeaderUserCode", textAlign: "center", width: "11%"},
+                {name: "担保方式", key: "guaranteeType", textAlign: "center", width: "10%"},
                 {name: "导入时间", key: "createTsAlia", textAlign: "center", width: "12%"}
             ]
         };
