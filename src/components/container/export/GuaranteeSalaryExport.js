ece074d2f6c1a9a4a2fb93159bcaee72a3f1a42d
xiaorui.guo <775870132@qq.com>
2017-10-14 20:20:49 +0800

    fix

--- src/components/container/export/GuaranteeSalaryExport.js
+++ src/components/container/export/GuaranteeSalaryExport.js
@@ -7,14 +7,15 @@ class GuaranteeSalaryExport extends ExportListPage {
         this.state.table.config = {
             column: [
                 {name: "公司", key: "corpName", textAlign: "center", width: "10%"},
-                {name: "大区编号", key: "regionCode", textAlign: "center", width: "12%"},
+                {name: "大区编号", key: "regionCode", textAlign: "center", width: "10%"},
                 {name: "大区", key: "regionName", textAlign: "center", width: "10%"},
-                {name: "店面编号", key: "storeCode", textAlign: "center", width: "12%"},
-                {name: "店面", key: "storeName", textAlign: "center", width: "12%"},
+                {name: "店面编号", key: "storeCode", textAlign: "center", width: "10%"},
+                {name: "店面", key: "storeName", textAlign: "center", width: "10%"},
                 {name: "姓名", key: "userName", textAlign: "center", width: "10%"},
-                {name: "入职时间", key: "onDutyTimeAlia", textAlign: "center", width: "12%"},
+                {name: "入职时间", key: "onDutyTimeAlia", textAlign: "center", width: "10%"},
                 {name: "系统号", key: "userCode", textAlign: "center", width: "10%"},
-                {name: "保障薪资", key: "guaranteeStanderSalary", textAlign: "center", width: "12%"},
+                {name: "基本保障薪资", key: "guaranteeStanderSalary", textAlign: "center", width: "10%"},
+                {name: "保障薪资上限", key: "guaranteeMaxSalary", textAlign: "center", width: "10%"},
             ]
         };
     }
