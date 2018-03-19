b27ca3601a168381781f439b418a4c1dd2d1b78f
lingjuanguan <lingjuanguan@gmail.com>
2018-03-19 23:03:17 +0800

    add

--- src/components/container/export/CommissionExport.js
+++ src/components/container/export/CommissionExport.js
@@ -7,14 +7,15 @@ class CommissionExport extends ExportListPage{
         this.state.table.config = {
             column: [
                 {name: "公司", key: "corpName", textAlign: "center", width: "10%"},
-                {name: "大区编号", key: "regionCode", textAlign: "center", width: "12%"},
+                {name: "大区编号", key: "regionCode", textAlign: "center", width: "10%"},
                 {name: "大区", key: "regionName", textAlign: "center", width: "10%"},
-                {name: "店面编号", key: "storeCode", textAlign: "center", width: "12%"},
-                {name: "店面", key: "storeName", textAlign: "center", width: "12%"},
+                {name: "店面编号", key: "storeCode", textAlign: "center", width: "10%"},
+                {name: "店面", key: "storeName", textAlign: "center", width: "10%"},
                 {name: "姓名", key: "userName", textAlign: "center", width: "10%"},
-                {name: "入职时间", key: "onDutyTime", textAlign: "center", width: "12%"},
+                {name: "入职时间", key: "onDutyTime", textAlign: "center", width: "10%"},
                 {name: "系统号", key: "userCode", textAlign: "center", width: "10%"},
-                {name: "提佣", key: "commissionAmount", textAlign: "center", width: "12%"},
+                {name: "本人提佣", key: "commissionAmount", textAlign: "center", width: "10%"},
+                {name: "带人提佣", key: "daiRenCommission", textAlign: "center", width: "10%"},
             ]
         };
     }
--- src/components/container/import/Status.js
+++ src/components/container/import/Status.js
@@ -10,15 +10,16 @@ class Status extends ImportListPage{
                 {name: "大区编码", key: "regionCode", textAlign: "center", width: "7%"},
                 {name: "大区", key: "regionName", textAlign: "center", width: "7%"},
                 {name: "店面编码", key: "storeCode", textAlign: "center", width: "7%"},
-                {name: "店面", key: "storeName", textAlign: "center", width: "7%"},
+                {name: "店面", key: "storeName", textAlign: "center", width: "6%"},
                 {name: "姓名", key: "userName", textAlign: "center", width: "6%"},
                 {name: "入职时间", key: "onDutyTimeAlia", textAlign: "center", width: "7%"},
                 {name: "系统号", key: "userCode", textAlign: "center", width: "5%"},
                 {name: "目前状态", key: "currentStatus", textAlign: "center", width: "6%"},
-                {name: "是否经纪人", key: "isJjrAlia", textAlign: "center", width: "8%"},
-                {name: "是否扣减基础分", key: "throwBaseScoreAlia", textAlign: "center", width: "11%"},
+                {name: "是否经纪人", key: "isJjrAlia", textAlign: "center", width: "7%"},
+                {name: "是否扣减基础分", key: "throwBaseScoreAlia", textAlign: "center", width: "7%"},
+                {name: "全勤考勤", key: "allDay", textAlign: "center", width: "7%"},
                 {name: "考勤天数", key: "attendanceDay", textAlign: "center", width: "7%"},
-                {name: "导入时间", key: "createTsAlia", textAlign: "center", width: "8%"},
+                {name: "导入时间", key: "createTsAlia", textAlign: "center", width: "7%"},
             ]
         };
         this.state.queryParams.importType = 'STATUS_ADJUST_IMPORT';
